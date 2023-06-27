fa48a51e12816fa70a5c8f6d6eefeca725fd2059
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-06-27 20:52:07 +0530

    Fix html

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,8 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @recipes = Recipe.all.where(public: true)
-    # @current_user = current_user
-    # @foods = current_user.recipes.includes!(:foods).map(&:foods).flatten.uniq
-    # @total_price = @foods.map { |ingredient| ingredient.price * ingredient.total_quantity_recipes }.sum
+    @recipes = Recipe.all.where(public: true).includes([:user])
   end
 end
