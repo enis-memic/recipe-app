9e38db1aa5ead92ffcc4f57dede2da9b163aa22f
Dheeraj Sachdeva <dheerajarya@gmail.com>
2023-06-27 15:26:57 +0530

    Add files for Recipe HTML

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,8 @@
 class PublicRecipesController < ApplicationController
   def index
     @recipes = Recipe.all.where(public: true)
+    # @current_user = current_user
+    # @foods = current_user.recipes.includes!(:foods).map(&:foods).flatten.uniq
+    # @total_price = @foods.map { |ingredient| ingredient.price * ingredient.total_quantity_recipes }.sum
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
  devise_for :users
 root 'public_recipes#index'
 
-  resources :recipes, only: [:index, :show, :create, :destroy] do
+  resources :recipes, only: [:index, :show, :create, :new, :destroy] do
     resources :recipe_foods, except: :update
   end
 
